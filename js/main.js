25afb27cd1382daeb07d52146a6d550bbe8e760a
SergeiBoy <s_shchetinin@mail.ru>
2020-06-08 19:20:40 +0500

    Сделали карточку объявления

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ var OFFER_QUANTITY = 8;
 var PIN_X_RANGE = [0, 1200];
 var PIN_Y_RANGE = [130, 630];
 var APARTMENT_TYPES = ['palace', 'flat', 'house', 'bungalo'];
+var APARTMENT_TYPES_TRANSLATION = ['Дворец', 'Квартира', 'Дом', 'Бунгало'];
 var MAX_ROOM_QUANTITY = 5;
 var MAX_GUEST_QUANTITY = 5;
 var CHECKIN_TIMES = ['12:00', '13:00', '14:00'];
@@ -17,9 +18,33 @@ var offers = [];
 var map = document.querySelector('.map');
 var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 var pinsOnMap = document.querySelector('.map__pins');
+var offerTemplate = document.querySelector('#card').content.querySelector('.map__card');
+var filter = map.querySelector('.map__filters-container');
 
 var getRandomNumber = function (number) {
   return Math.floor(Math.random() * (number + 1));
+};
+
+var formatNounPluralForm = function (number, one, two, many) {
+  var mod10 = number % 10;
+  var mod100 = number % 100;
+
+  switch (true) {
+    case (mod100 >= 11 && mod100 <= 20):
+      return many;
+
+    case (mod10 > 5):
+      return many;
+
+    case (mod10 === 1):
+      return one;
+
+    case (mod10 >= 2 && mod10 <= 4):
+      return two;
+
+    default:
+      return many;
+  }
 };
 
 var createOffer = function (i) {
@@ -68,6 +93,62 @@ var renderPin = function (offering) {
   return pinElement;
 };
 
+var renderOffer = function (offering) {
+  var offerElement = offerTemplate.cloneNode(true);
+  var roomsForGuests = offerElement.querySelector('.popup__text--capacity');
+  var checkinCheckout = offerElement.querySelector('.popup__text--time');
+  var featuresList = offerElement.querySelectorAll('.popup__feature');
+  var imgList = offerElement.querySelector('.popup__photos');
+  var img = imgList.querySelector('.popup__photo');
+
+  offerElement.querySelector('.popup__title').textContent = offering.offer.title;
+  offerElement.querySelector('.popup__text--address').textContent = offering.offer.address;
+
+  if (offering.offer.price) {
+    offerElement.querySelector('.popup__text--price').innerHTML = '&#x20bd;<span>/ночь</span>';
+    offerElement.querySelector('.popup__text--price').prepend(offering.offer.price);
+  } else {
+    offerElement.querySelector('.popup__text--price').innerHTML = '';
+  }
+
+  offerElement.querySelector('.popup__type').textContent = APARTMENT_TYPES_TRANSLATION[APARTMENT_TYPES.indexOf(offering.offer.type)];
+
+  roomsForGuests.textContent = '';
+  if (offering.offer.rooms) {
+    roomsForGuests.textContent = offering.offer.rooms + ' ' + formatNounPluralForm(offering.offer.rooms, 'комната', 'комнаты', 'комнат') + ' ';
+  }
+  if (offering.offer.guests) {
+    roomsForGuests.textContent += 'для ' + offering.offer.guests + ' ' + formatNounPluralForm(offering.offer.guests, 'гостя', 'гостей', 'гостей');
+  }
+
+  checkinCheckout.textContent = '';
+  if (offering.offer.checkin) {
+    checkinCheckout.textContent = 'Заезд после ' + offering.offer.checkin + ', ';
+  }
+  if (offering.offer.checkout) {
+    checkinCheckout.textContent += 'выезд до ' + offering.offer.checkout;
+  }
+
+  for (var i = 0; i < FEATURES.length; i++) {
+    if (offering.offer.features.indexOf(FEATURES[i]) < 0) {
+      featuresList[i].remove();
+    }
+  }
+
+  offerElement.querySelector('.popup__description').textContent = offering.offer.description;
+
+  img.remove();
+  for (var j = 0; j < offering.offer.photos.length; j++) {
+    var imgElement = img.cloneNode(true);
+    imgElement.src = offering.offer.photos[j];
+    imgList.appendChild(imgElement);
+  }
+
+  offerElement.querySelector('.popup__avatar').src = offering.author.avatar;
+
+  return offerElement;
+};
+
 var renderPins = function () {
   var fragment = document.createDocumentFragment();
   for (var i = 0; i < offers.length; i++) {
@@ -79,3 +160,5 @@ var renderPins = function () {
 createOffers();
 map.classList.remove('map--faded');
 renderPins();
+
+filter.before(renderOffer(offers[0]));
