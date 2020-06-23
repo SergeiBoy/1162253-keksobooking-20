29288144c9567a38838a9c22c73d95dcb6c43f6d
SergeiBoy <s_shchetinin@mail.ru>
2020-06-23 12:30:53 +0500

    Получаем данные с сервера

--- js/main.js
+++ js/main.js
@@ -3,7 +3,10 @@
 (function () {
   var activatePage = function () {
     window.map.map.classList.remove('map--faded');
-    window.map.renderPins();
+
+    window.backend.load(function (offers) {
+      window.map.renderPins(offers);
+    }, window.backend.onError);
 
     window.form.setFormsDisabled(false);
   };
