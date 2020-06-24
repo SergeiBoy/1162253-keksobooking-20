f4368c5420010f385218f27d5257693084896c28
SergeiBoy <s_shchetinin@mail.ru>
2020-06-24 19:49:54 +0500

    Исправление замечания - выделение функции sendRequest

--- js/backend.js
+++ js/backend.js
@@ -8,7 +8,7 @@
   };
   var TIMEOUT_IN_MS = 10000;
 
-  var save = function (data, onLoad, onError) {
+  var sendRequest = function (onLoad, onError, data) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
 
@@ -28,32 +28,21 @@
 
     xhr.timeout = TIMEOUT_IN_MS;
 
-    xhr.open('POST', saveURL);
-    xhr.send(data);
+    if (data) {
+      xhr.open('POST', saveURL);
+      xhr.send(data);
+    } else {
+      xhr.open('GET', loadURL);
+      xhr.send();
+    }
+  };
+
+  var save = function (data, onLoad, onError) {
+    sendRequest(onLoad, onError, data);
   };
 
   var load = function (onLoad, onError) {
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-
-    xhr.addEventListener('load', function () {
-      if (xhr.status === StatusCode.OK) {
-        onLoad(xhr.response);
-      } else {
-        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-      }
-    });
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
-    });
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-    });
-
-    xhr.timeout = TIMEOUT_IN_MS;
-
-    xhr.open('GET', loadURL);
-    xhr.send();
+    sendRequest(onLoad, onError);
   };
 
   var onError = function (errorMessage) {
